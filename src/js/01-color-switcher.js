31fcb5d1bd484ec704fec7430b5c6da0778032bf
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-06 20:02:35 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+};
+const DELEY = 1000;
+let intervalId = null;
 
+refs.btnStart.addEventListener('click', onStart)
+refs.btnStop.addEventListener('click', onStop)
+
+function onStart(){
+intervalId =  setInterval(() => {
+refs.body.style.background =  getRandomHexColor()
+}, DELEY);
+
+refs.btnStart.setAttribute('disabled', true);
+refs.btnStop.removeAttribute("disabled");
+};
+
+function onStop(){
+    clearInterval(intervalId);
+    
+    refs.btnStop.setAttribute('disabled', true);
+    refs.btnStart.removeAttribute("disabled"); 
+};
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
