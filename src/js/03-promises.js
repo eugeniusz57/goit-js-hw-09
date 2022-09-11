4afade4d2fc05497a31324820ff65b19f7adaf34
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-11 16:00:10 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,22 +1,43 @@
+import {refs} from './03-refs';
 
+import Notiflix from 'notiflix';
 
-const refs ={
-  btnCreate: document.querySelector('button'),
-  inputDelay: document.querySelector('input[name="delay"]'),
-  inputStep: document.querySelector('input[name="step"]'),
-  inputAmount: document.querySelector('input[name="amount"]'),
-  form: document.querySelector('.form')
+const position = refs.inputDelay.value;
+console.log(position);
+
+refs.form.addEventListener('input',  onInputObject,);
+refs.form.addEventListener('submit', onSubmit);
+
+const Date ={};
+
+function onInputObject(e){
+Date[e.target.name] = e.target.value;
 }
 
-console.log(refs.form);
+function onSubmit(e){
+  e.preventDefault();
 
+  const amount = Number(Date.amount);
+  let delay = Number(Date.delay);
 
+  for (let i = 1, delay =  Number(Date.delay); i <= amount;  i+=1, delay += Number(Date.step) ){
+    setTimeout(() => {
+      createPromise(i, delay)
+    }, delay);
+  }
+};
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
---
+++ src/js/03-refs.js
@@ -0,0 +1,7 @@
+export const refs ={
+    btnCreate: document.querySelector('button'),
+    inputDelay: document.querySelector('input[name="delay"]'),
+    inputStep: document.querySelector('input[name="step"]'),
+    inputAmount: document.querySelector('input[name="amount"]'),
+    form: document.querySelector('.form')
+  }
