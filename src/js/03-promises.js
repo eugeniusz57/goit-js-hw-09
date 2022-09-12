562a511e532b006b59a27242c650a4c8dc7d93c4
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-12 18:01:56 +0200

    change task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,18 +17,25 @@ function onSubmit(e){
   const amount = Number(Date.amount);
   let delay = Number(Date.delay);
 
-  for (let i = 1, delay =  Number(Date.delay); i <= amount;  i+=1, delay += Number(Date.step) ){
-    setTimeout(() => {
-      createPromise(i, delay)
-    }, delay);
-  }
+  if(Number(Date.delay) < 0 || Number(Date.step) < 0 || Number(Date.amount) < 0){
+    Notiflix.Notify.failure(`❌ The value in the field cannot be negative`);
+    return;
+  } for (let i = 1, delay =  Number(Date.delay); i <= amount;  i+=1, delay += Number(Date.step) ){
+  setTimeout(() => {
+    createPromise(i, delay).then(result => console.log(result)).catch(error => console.log(error))
+  }, delay);
+}
+  
 };
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    } else {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    }
+  })
+ 
 }
